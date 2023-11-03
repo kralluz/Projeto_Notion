f623ca36d53b5ae1dab9a939cd706c78cc578638
VitorSantosDev <123842676+Vitorsantosdomingues@users.noreply.github.com>
2023-11-02 22:20:24 -0300

    criando sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './styles/index.scss';
+import './App.css';
+import { SideBar } from './components/sidebar'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='min-h-screen text-zinc-900 bg-gradient-to-tl from-gray-200 via-gray-400 to-gray-600'>
+        <div className='bg-white w-[1100px] mx-auto rounded-xl min-h-[200px] shadow-sm border border-black/20'>
+          <SideBar/>        
+          <main>
+            <p>awdwd</p>
+            <h1>awndaodnáo</h1>
+          </main>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
