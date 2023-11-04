80ec41fdbe9a0a3f0da8f0a2e5ba5aa6cadeb880
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-11-03 22:06:28 -0300

    uninstall depenences
    indent app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,15 @@ import { SideBar } from "./components/sidebar/sidebar.container";
 import { Main } from "./components/main/main.component";
 import { AppStyled } from "./App.styles";
 
-
 function App() {
-  return (
-    <>
-     
-       <AppStyled>
-          <SideBar />
-          <Main/>
-       </AppStyled>
-          
-      
-     
-      
-    </>
-  )
+    return (
+        <>
+            <AppStyled>
+                <SideBar />
+                <Main />
+            </AppStyled>
+        </>
+    );
 }
 
 export default App;
